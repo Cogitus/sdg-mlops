0db0a1a128559bd580ef14c43d71d3e88ac6263f
Ariel Alsina <ariel.alsina2007@gmail.com>
2023-01-31 23:33:57 -0300

    start: make first skelleton of the `tokenize_data`
    this was reflected at the `split_data/tokenization.py` file

--- split_data/tokenization.py
+++ split_data/tokenization.py
@@ -1,5 +1,15 @@
+import shutil
+from pathlib import Path
+
 import en_core_web_lg
+import nltk
 import spacy
 
-nlp = en_core_web_lg.load()
-doc = nlp(u"This is a sentence.")
+# Set the location of the NLTK data directory to a custom folder
+nltk_data_folder = Path.cwd() / "nltk_data"
+
+nltk.download("punkt", download_dir=nltk_data_folder)
+nltk.download("stopwords", download_dir=nltk_data_folder)
+
+
+shutil.rmtree(nltk_data_folder)
