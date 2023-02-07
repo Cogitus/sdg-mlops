0cce1d4b190fdf18d4783aa280b4473f09ea6e5f
Ariel Alsina <ariel.alsina2007@gmail.com>
2023-02-06 23:54:01 -0300

    refactor: substitute `shutil.rmtree()` by `tempdir.TemporaryDirectory()`
    Since the nltk will be deleted just after being used, it is easier
    to simply store all the data to a temporary directory and just wait
    for its deletation after the code execution.

--- split_data/tokenization.py
+++ split_data/tokenization.py
@@ -1,15 +1,30 @@
-import shutil
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
-import en_core_web_lg
 import nltk
-import spacy
-
-# Set the location of the NLTK data directory to a custom folder
-nltk_data_folder = Path.cwd() / "nltk_data"
-
-nltk.download("punkt", download_dir=nltk_data_folder)
-nltk.download("stopwords", download_dir=nltk_data_folder)
+from utils.data_loader import LocalSDGLoader
 
 
-shutil.rmtree(nltk_data_folder)
+def main() -> None:
+    # current_dir = Path.cwd()
+
+    data_loader = LocalSDGLoader()
+
+    with TemporaryDirectory() as tmp_dir:
+        # Set the location of the NLTK data directory to a custom folder
+        nltk_data_folder = Path(tmp_dir) / "nltk_data"
+
+        nltk.download("punkt", download_dir=nltk_data_folder)
+        nltk.download("stopwords", download_dir=nltk_data_folder)
+
+        arquivos = data_loader.load_data(
+            data_location=Path(
+                "/home/alsinaariel/Downloads/SDGs-20230122T202630Z-001/SDGs"
+            )
+        )
+
+        print(arquivos)
+
+
+if __name__ == "__main__":
+    main()
