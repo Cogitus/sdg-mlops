3c89d2ead90737cc56ccb2181f415ab22b9e87bf
Ariel Alsina <ariel.alsina2007@gmail.com>
2023-02-06 23:50:50 -0300

    chores: change `split_data/conda.yml' python to 3.10
    Added some versions other libs to `split_data/conda.yml`:
        - numpy
        - pandas
        - scikit-learn
        - spacy
        - tensorflow
    
    And fixed the log error message of the `download_spacy_model.py`

--- split_data/download_spacy_model.py
+++ split_data/download_spacy_model.py
@@ -14,9 +14,7 @@ if __name__ == "__main__":
     try:
         nlp = spacy.load("en_core_web_lg")
     except OSError:  # If not present, we download
-        logger.error(
-            "spacy model `en_core_web_lg` is already installed, start donwload"
-        )
+        logger.error("spacy model `en_core_web_lg` is not installed, starting donwload")
         spacy.cli.download("en_core_web_lg")
     else:
         logger.info(
