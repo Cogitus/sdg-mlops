f4bb4dc3b9491b8ba394850ef24f5be87169ff14
Ariel Alsina <ariel.alsina2007@gmail.com>
2023-03-29 15:20:53 -0300

    refactor: change disposition of itens in `run_configurations.yaml`

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ def run(configuration: DictConfig) -> None:
             uri=os.path.join(ROOT_PATH, "preprocess_data"),
             entry_point="main",
             parameters={
-                "titles": configuration["data"]["wandb"]["tag"]["titles"],
-                "authors": configuration["data"]["wandb"]["tag"]["authors"],
-                "affiliations": configuration["data"]["wandb"]["tag"]["affiliations"],
-                "dois": configuration["data"]["wandb"]["tag"]["dois"],
-                "keywords": configuration["data"]["wandb"]["tag"]["keywords"],
-                "abstracts": configuration["data"]["wandb"]["tag"]["abstracts"],
+                "titles": configuration["wandb"]["tag"]["titles"],
+                "authors": configuration["wandb"]["tag"]["authors"],
+                "affiliations": configuration["wandb"]["tag"]["affiliations"],
+                "dois": configuration["wandb"]["tag"]["dois"],
+                "keywords": configuration["wandb"]["tag"]["keywords"],
+                "abstracts": configuration["wandb"]["tag"]["abstracts"],
             },
         )
 
@@ -70,9 +70,11 @@ def run(configuration: DictConfig) -> None:
             {
                 "entry_point": "dataset_balancing",
                 "parameters": {
-                    "quantile": configuration["data"]["balancing"]["quantile"],
-                    "random_state": configuration["data"]["balancing"]["random_state"],
-                    "path_sdg_dataset": configuration["data"]["splitting"][
+                    "quantile": configuration["split_data"]["balancing"]["quantile"],
+                    "random_state": configuration["split_data"]["balancing"][
+                        "random_state"
+                    ],
+                    "path_sdg_dataset": configuration["split_data"]["splitting"][
                         "path_sdg_dataset"
                     ],
                 },
@@ -80,31 +82,31 @@ def run(configuration: DictConfig) -> None:
             {
                 "entry_point": "preprocess_and_split",
                 "parameters": {
-                    "train_size_factor": configuration["data"]["splitting"][
+                    "train_size_factor": configuration["split_data"]["splitting"][
                         "train_size_factor"
                     ],
-                    "dataset_name": configuration["data"]["wandb"]["tag"][
-                        "balanced_dataset"
-                    ],
-                    "test_share_size": configuration["data"]["splitting"][
+                    "dataset_name": configuration["wandb"]["tag"]["balanced_dataset"],
+                    "test_share_size": configuration["split_data"]["splitting"][
                         "test_share_size"
                     ],
-                    "random_state": configuration["data"]["splitting"]["random_state"],
+                    "random_state": configuration["split_data"]["splitting"][
+                        "random_state"
+                    ],
                 },
             },
             {
                 "entry_point": "advanced_preprocessing",
                 "parameters": {
-                    "X_train": configuration["data"]["wandb"]["tag"]["X_train"],
-                    "y_train": configuration["data"]["wandb"]["tag"]["y_train"],
-                    "X_valid": configuration["data"]["wandb"]["tag"]["X_valid"],
-                    "y_valid": configuration["data"]["wandb"]["tag"]["y_valid"],
-                    "X_test": configuration["data"]["wandb"]["tag"]["X_test"],
-                    "y_test": configuration["data"]["wandb"]["tag"]["y_test"],
-                    "tf_batch_size": configuration["data"]["advanced_preprocessing"][
-                        "tf_batch_size"
-                    ],
-                    "tf_seed": configuration["data"]["advanced_preprocessing"][
+                    "X_train": configuration["wandb"]["tag"]["X_train"],
+                    "y_train": configuration["wandb"]["tag"]["y_train"],
+                    "X_valid": configuration["wandb"]["tag"]["X_valid"],
+                    "y_valid": configuration["wandb"]["tag"]["y_valid"],
+                    "X_test": configuration["wandb"]["tag"]["X_test"],
+                    "y_test": configuration["wandb"]["tag"]["y_test"],
+                    "tf_batch_size": configuration["split_data"][
+                        "advanced_preprocessing"
+                    ]["tf_batch_size"],
+                    "tf_seed": configuration["split_data"]["advanced_preprocessing"][
                         "tf_seed"
                     ],
                 },
